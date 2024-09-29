cdec7aec332ca7727c1ce9b0c1fa48e57879dee9
mg3994 <manishgautammg7@yahoo.com>
2024-09-29 11:30:28 +0530

    starter template

---
+++ android/app/src/main/kotlin/com/antinna/my_flutter_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.antinna.my_flutter_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/features.dart
@@ -0,0 +1,3 @@
+export 'package:themer/themer.dart';
+export 'package:updater/updater.dart';
+export 'package:l10n/l10n.dart';
